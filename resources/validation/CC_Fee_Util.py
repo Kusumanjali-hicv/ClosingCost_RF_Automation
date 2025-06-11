9423bbab388f2932316589092edbbded21b49060
Kusumanjali-hicv <km@holidayinnclub.com>
2025-06-11 20:50:38 +0530

    round off mismatch

--- resources/validation/CC_Fee_Util.py
+++ resources/validation/CC_Fee_Util.py
@@ -1,6 +1,7 @@
 import json
 from robot.api.deco import keyword
 from robot.api import logger
+from decimal import Decimal, ROUND_HALF_UP
 
 json_file_path = 'config/rate_chart.json'
 
@@ -138,3 +139,13 @@ if __name__ == "__main__":
 #print(getFeeDetails("Deed Of Trust Release Fee-TX", "{\"closingCosts\":{\"titleInsuranceFee\":0.00,\"fees\":[{\"amount\":53.50,\"description\":\"Deed Recording Fee\",\"transactionCode\":\"Deed Recording Fee\",\"payableTo\":\"Orange County Comptroller\"},{\"amount\":575.00,\"description\":\"Owner's Title Fee\",\"transactionCode\":\"Owner's Title Fee\",\"payableTo\":\"Wilson Title Services, LLC\"},{\"amount\":0.00,\"description\":\"Intangible Tax - Note\",\"transactionCode\":\"Intangible Tax - Note\",\"payableTo\":\"Orange County Comptroller\"},{\"amount\":25.00,\"description\":\"Lender's Title Insurance\",\"transactionCode\":\"Lender's Title Insurance\",\"payableTo\":\"Wilson Title Services\"},{\"amount\":70.00,\"description\":\"Documentary Stamps - Mortgage\",\"transactionCode\":\"Documentary Stamps - Mortgage\",\"payableTo\":\"Orange County Comptroller\"},{\"amount\":199.00,\"description\":\"Club Membership Fee\",\"transactionCode\":\"Club Membership Fee\",\"payableTo\":\"Club Membership Fee\"},{\"amount\":169.00,\"description\":\"Club Enrollment Fee\",\"transactionCode\":\"Club Enrollment Fee\",\"payableTo\":\"Club Enrollment Fee\"},{\"amount\":389.00,\"description\":\"Club Registration Fee\",\"transactionCode\":\"Club Registration Fee\",\"payableTo\":\"Club Registration Fee\"},{\"amount\":100.00,\"description\":\"HOA Set Up Fee\",\"transactionCode\":\"HOA Set Up Fee\",\"payableTo\":\"Orange Lake Trust Owners’ Association\"},{\"amount\":130.00,\"description\":\"Document Preparation Fee - Refinance\",\"transactionCode\":\"Document Preparation Fee - Refinance\",\"payableTo\":\"Holiday Inn Club Vacations Incorporated\"},{\"amount\":399.00,\"description\":\"Origination Fee - Refinance\",\"transactionCode\":\"Origination Fee - Refinance\",\"payableTo\":\"Holiday Inn Club Vacations Incorporated\"},{\"amount\":371.00,\"description\":\"Maintenance Fee\",\"transactionCode\":\"Maintenance Fee\",\"payableTo\":\"Maintenance Fee\"},{\"amount\":4.00,\"description\":\"Guarantee Fee-TX\",\"transactionCode\":\"Guarantee Fee-TX\",\"payableTo\":\"Texas Title Guaranty Association\"},{\"amount\":0.00,\"description\":\"Escrow Fee-AZ\",\"transactionCode\":\"Escrow Fee-AZ\",\"payableTo\":\"Wilson Title Agency Services, LLC\"},{\"amount\":37.00,\"description\":\"Deed Of Trust Recording Fee-TX\",\"transactionCode\":\"Deed Of Trust Recording Fee-TX\",\"payableTo\":\"Montogomery County Clerk's Office\"},{\"amount\":29.00,\"description\":\"Deed Of Trust Release Fee-TX\",\"transactionCode\":\"Deed Of Trust Release Fee-TX\",\"payableTo\":\"Montogomery County Clerk's Office\"}],\"grandTotal\":2550.50,\"documentaryStamps\":0.00,\"escrowAgentFee\":0.00,\"intangibleTax\":0.00},\"contractId\":\"FS50MJ9VB5W0I7X6DT\",\"siteId\":\"9999\",\"brand\":\"HICV\"}"))
 
 
+
+def round_fee(fee):
+    value = Decimal(str(fee))
+    rounded_value = value.quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
+    return rounded_value
+
+#fee = 76.475
+
+
+#print(f"fee is {fee}, round = {round_fee(fee)}")
